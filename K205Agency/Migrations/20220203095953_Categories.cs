5f09a8cc1dec80c266185c29f53f87c8516abdec
Пользователь <aytacrustmli93@gmail.com>
2022-02-03 22:18:19 +0400

    03.02.2022

---
+++ K205Agency/Migrations/20220203095953_Categories.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace K205Agency.Migrations
+{
+    public partial class Categories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Portfolios_Category_CategoryID",
+                table: "Portfolios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Portfolios_Categories_CategoryID",
+                table: "Portfolios",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Portfolios_Categories_CategoryID",
+                table: "Portfolios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Portfolios_Category_CategoryID",
+                table: "Portfolios",
+                column: "CategoryID",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
